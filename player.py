df37130403f27599913661a7d9942c526a022655
Buymaew <sabaimaew0@gmail.com>
2023-10-07 06:34:47 +0700

    update

--- player.py
+++ player.py
@@ -2,7 +2,8 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def ___init__(self,pos,groups):
+    def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../pic/rock.png').convert_alpha()
+        img = pygame.image.load('../Zelda_Pygame/pic/player.png')
+        self.image = pygame.transform.scale(img, (100,100)).convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
