a81cc5f2be255470f4298ce60b5d0e0c5caaf328
Buymaew <sabaimaew0@gmail.com>
2023-10-07 14:32:49 +0700

    update

--- player.py
+++ player.py
@@ -5,5 +5,5 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
         img = pygame.image.load('../Zelda_Pygame/pic/player.png')
-        self.image = pygame.transform.scale(img, (100,100)).convert_alpha()
+        self.image = pygame.transform.scale(img, (50,50)).convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
